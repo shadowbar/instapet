bca1eac38e18711f9a9be6fd0874db6ef359663b
barmolot <barm@avanan.com>
2019-02-07 21:59:14 +0200

    Initial Commit

---
+++ instapet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instapet
+//
+//  Created by Bar Molot on 07/02/2019.
+//  Copyright © 2019 colman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
